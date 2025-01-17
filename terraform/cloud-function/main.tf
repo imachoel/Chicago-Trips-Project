4f2567fb561bba75d4cbea5c82c003ae8f54e564
imachoel <choukariimen@gmail.com>
2025-01-17 17:48:10 +0000

    test

--- terraform/cloud-function/main.tf
+++ terraform/cloud-function/main.tf
@@ -6,7 +6,7 @@ resource "google_cloudfunctions_function" "weather_function" {
   region      = "us-central1"
   entry_point = "fetch_weather_data"
 
-  source_archive_bucket = google_storage_bucket.function_bucket.name
+  source_archive_bucket = "${var.project_id}-function-bucket"
   source_archive_object = google_storage_bucket_object.function_zip.name
 
   trigger_http = true
